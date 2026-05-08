2db0d33beea8ad36750ab066f0c5a168acc434c5
Jackson Tiller <jackson@verdantbank.com>
2026-05-08 16:24:43 +0100

    Add Netflix bill payment transaction of ,105.20 and update balance to ,928.73

--- script.js
+++ script.js
@@ -11,7 +11,7 @@
 /* ============================================
    GLOBAL STATE & VARIABLES
    ============================================ */
-var currentBal = 707113.73;  // Current account balance
+var currentBal = 550928.73;  // Current account balance
 var pinEntry = '';       // Transfer PIN entry
 var transferData = {};   // Stores current transfer data
 
